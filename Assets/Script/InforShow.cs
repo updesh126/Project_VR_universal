5e2c833f5cd28b2afc921fd8a3eb80bdee0f66ca
Updesh Pr. Singh <updesh071@gmail.com>
2022-09-30 23:23:03 +0530

    h

---
+++ Assets/Script/InforShow.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InforShow : MonoBehaviour
+{
+    public GameObject InfoObject;
+    private bool Show = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+    public void ShowHideanInfo()
+    {
+        if (!Show)
+        {
+            InfoObject.SetActive(true);
+            Show = true;
+        }
+        else
+        {
+            InfoObject.SetActive(false);
+        }
+    }
+
+}
